d922a1840fea3960da891ceda3b2331187a900eb
Amelia <adowns@pivotal.io>
2016-05-06 15:52:06 -0700

    refactor templates
    Signed-off-by: Urvashi <ureddy@pivotal.io>

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,18 +1,25 @@
 class ResultsController < ApplicationController
   def cat_success
     @names = params[:message] || 'human'
+    @image = 'https://goo.gl/7d5h8V'
+    render :results
   end
 
   def cat_fail
     @names = params[:message] || 'human'
+    @image = 'http://goo.gl/XxuaWA'
+    render :results
   end
 
   def default_success
     @names = params[:message] || 'human'
+    @image = 'https://goo.gl/haz4z1'
+    render :results
   end
 
   def default_fail
     @names = params[:message] || 'human'
+    @image = 'https://goo.gl/Cg40iF'
+    render :results
   end
-
 end
